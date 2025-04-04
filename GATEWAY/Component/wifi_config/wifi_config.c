500d48eb22a84587107c4c516cf4478debc35134
TranTu67750205 <trananhtu121201@gmail.com>
2025-04-05 04:09:19 +0700

    Cập nhật tính năng mới

--- GATEWAY/Component/wifi_config/wifi_config.c
+++ GATEWAY/Component/wifi_config/wifi_config.c
@@ -39,8 +39,8 @@ void task_wifi_init(){
 
     wifi_config_t sta_cfg = {
         .sta = {
-            .ssid = "Galaxy A05s 6247",                    // 9999     Galaxy A05s 6247      VNPT_Hanh
-            .password = "67750205tat",                        // quangduc9999         67750205tat        01298649287       
+            .ssid = "VNPT_Hanh",                    // 9999     Galaxy A05s 6247      VNPT_Hanh
+            .password = "01298649287",                        // quangduc9999         67750205tat        01298649287       
             .threshold.authmode = WIFI_AUTH_WPA2_PSK,
         }
     };
--- GATEWAY/main/GATEWAY.c
+++ GATEWAY/main/GATEWAY.c
@@ -34,6 +34,8 @@
 //
 #include "lora.h"
 #include "wifi_config.h"
+#include "SUB_MQTT.h"
+
 
 
 static const char *TAG = "LORA_receive";
@@ -53,39 +55,11 @@ static const char *TAG = "LORA_receive";
 //ESP_LOGI(TAG, "turn ON LED");
 //gpio_set_level(LED_PIN, 1);
 
+//WIFI TASK
 
 
-
-// Hàm khởi tạo các chân LoRa và LED
-void lora_init_pins() {
-    esp_rom_gpio_pad_select_gpio(LORA_CS);
-    gpio_set_direction(LORA_CS, GPIO_MODE_OUTPUT);
-    gpio_set_level(LORA_CS, 1);
-
-    esp_rom_gpio_pad_select_gpio(LORA_RST);
-    gpio_set_direction(LORA_RST, GPIO_MODE_OUTPUT);
-    gpio_set_level(LORA_RST, 1);
-
-    esp_rom_gpio_pad_select_gpio(LED_PIN);
-    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
-    gpio_set_level(LED_PIN, 0); // Đèn tắt ban đầu
-}
-
-
-
-void app_main(void)
-{
-    lora_init_pins();
-    lora_init();
-    lora_set_frequency(435E6);
-    lora_enable_crc();
-    lora_set_bandwidth(125E3);  // Băng thông
-    lora_set_spreading_factor(7);  // Spread Factor
-    lora_set_coding_rate(5); // Coding Rate 4/5
-    lora_receive();  
-
-    printf("LoRa Receiver Đã Khởi Động\n");
-
+//LORA TASK
+void lora_task (){
     while (1) {
         uint8_t buf[128];
         //emset(buf, 0, 129);
@@ -96,9 +70,11 @@ void app_main(void)
             ESP_LOGI(TAG, "NHẬN ĐƯỢC GÓI TIN: %s\n", buf);
             // Tách ID và trạng thái từ gói tin
             char node_id [10];
-            char led_state[10]; // Mảng ký tự để chứa trạng thái
-            if (sscanf((char *)buf, "ID%[^:]:%s", node_id, led_state) == 2) {
-                printf("Nhận từ Node %s : Trạng thái = %s\n", node_id, led_state);
+            char led_state[10];
+            float soil, uv, temp, hum;;
+            if (sscanf((char *)buf, "ID%[^:]:TEMP=%f:HUM=%f:SOIL=%f:UV=%f", node_id, &temp, &hum, &soil, &uv) == 5) {
+                ESP_LOGI(TAG, "Node ID: %s, Temp: %.1f, Hum: %.1f, Soil: %.1f, UV: %.1f", node_id, temp, hum, soil, uv);
+   
                 if (strcmp((char *)node_id, "END123456" ) == 0) {
                     if (strcmp((char *)led_state, "ON") == 0 ) {
                         gpio_set_level(LED_PIN, 1); 
@@ -125,5 +101,59 @@ void app_main(void)
         }
         vTaskDelay(pdMS_TO_TICKS(500)); // Giảm tải CPU
     }
+}
+
+// Hàm khởi tạo các chân LoRa và LED
+void lora_init_pins() {
+    esp_rom_gpio_pad_select_gpio(LORA_CS);
+    gpio_set_direction(LORA_CS, GPIO_MODE_OUTPUT);
+    gpio_set_level(LORA_CS, 1);
+
+    esp_rom_gpio_pad_select_gpio(LORA_RST);
+    gpio_set_direction(LORA_RST, GPIO_MODE_OUTPUT);
+    gpio_set_level(LORA_RST, 1);
+
+    esp_rom_gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(LED_PIN, 0); // Đèn tắt ban đầu
+}
+
+
+
+void app_main(void)
+{
+    //loralora
+    lora_init_pins();
+    lora_init();
+    lora_set_frequency(435E6);
+    lora_enable_crc();
+    lora_set_bandwidth(125E3);  // Băng thông
+    lora_set_spreading_factor(7);  // Spread Factor
+    lora_set_coding_rate(5); // Coding Rate 4/5
+    lora_receive();  
+    xTaskCreate(&lora_task, "lora_task", 4096, NULL, 3, NULL);
+    printf("LoRa Receiver Đã Khởi Động\n");
+
+    //task wifi 
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    task_wifi_init();
+
+    //mqtt tasktask
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
+    esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
+    esp_log_level_set("mqtt_example", ESP_LOG_VERBOSE);
+    esp_log_level_set("transport_base", ESP_LOG_VERBOSE);
+    esp_log_level_set("transport", ESP_LOG_VERBOSE);
+    esp_log_level_set("outbox", ESP_LOG_VERBOSE);
+
+    mqtt_app_start();
 
 }
